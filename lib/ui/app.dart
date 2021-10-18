90c406f786fb8ee533fb3f20c0efb001cff1f794
mleczny <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>
2021-10-18 14:31:46 +0200

    refactor ui structure

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tech_challenge_flutter/ui/search_repo/search_repo_page.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(home: SearchRepoPage());
   }
 }
---
+++ lib/ui/issues/issues_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IssuesPage extends StatelessWidget {
+  const IssuesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
