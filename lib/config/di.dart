c6656e15749eb519c3f35e0a2c1293d087de8134
mleczny <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>
2021-10-19 19:43:32 +0200

    updated di and main function

--- lib/config/di.dart
+++ lib/config/di.dart
@@ -2,12 +2,16 @@ import 'dart:async';
 
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:tech_challenge_flutter/config/api_config.dart';
 
 import 'di.config.dart';
 
 final GetIt _getIt = GetIt.instance;
 
 @InjectableInit()
-Future<void> configureDI() async => $initGetIt(_getIt);
+Future<void> configureDI(ApiConfig apiConfig) async {
+  _getIt.registerSingleton<ApiConfig>(apiConfig);
+  $initGetIt(_getIt);
+}
 
 T inject<T extends Object>() => _getIt.get<T>();
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:tech_challenge_flutter/config/api_config.dart';
+import 'package:tech_challenge_flutter/config/di.dart';
 import 'package:tech_challenge_flutter/ui/app.dart';
 
-void runMain(ApiConfig apiConfig) {
+Future<void> runMain(ApiConfig apiConfig) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await configureDI(apiConfig);
   runApp(const App());
 }
