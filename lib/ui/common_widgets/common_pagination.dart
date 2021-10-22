d0f4e5e7e245b7da11f1ffb3d6e65438b22ac2a8
mleczny <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>
2021-10-22 13:44:05 +0200

    fixed pagination component

--- lib/ui/common_widgets/common_pagination.dart
+++ lib/ui/common_widgets/common_pagination.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CommonPagination extends StatelessWidget {
+class CommonPagination extends StatefulWidget {
   final int numberOfPages;
   final Function(int index) onPagePressed;
   final int currentPage;
@@ -13,39 +13,105 @@ class CommonPagination extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CommonPagination> createState() => _CommonPaginationState();
+}
+
+class _CommonPaginationState extends State<CommonPagination> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (numberOfPages <= 1) return const SizedBox();
+    if (widget.numberOfPages <= 1) return const SizedBox();
 
     return SizedBox(
       height: 40,
       child: Padding(
-        padding: const EdgeInsets.only(left: 16.0, bottom: 16, right: 16),
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: numberOfPages,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) return const SizedBox();
-            return Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: InkWell(
-                onTap: () => onPagePressed(index),
-                child: Container(
-                  height: 25,
-                  width: 25,
-                  color: Theme.of(context).cardColor,
-                  child: Center(
-                    child: Text(
-                      '$index',
-                      style: TextStyle(
-                        color: index == currentPage ? Colors.white70 : Theme.of(context).disabledColor,
-                        fontWeight: index == currentPage ? FontWeight.bold : FontWeight.normal,
-                      ),
-                    ),
-                  ),
+        padding: const EdgeInsets.only(left: 16.0, bottom: 16, right: 8),
+        child: Row(
+          children: [
+            _SinglePage(
+              onPagePressed: (index) {
+                _scrollController.jumpTo(_scrollController.position.minScrollExtent);
+                widget.onPagePressed(index);
+              },
+              currentPage: widget.currentPage,
+              index: 1,
+            ),
+            Expanded(
+              child: ListView.builder(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.numberOfPages,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0 || index == 1 || index == widget.numberOfPages) return const SizedBox();
+                  return _SinglePage(
+                      onPagePressed: widget.onPagePressed, currentPage: widget.currentPage, index: index);
+                },
+              ),
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Text('  ...  ', style: TextStyle(color: Theme.of(context).disabledColor))),
+            _SinglePage(
+              onPagePressed: (index) async {
+                _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+                widget.onPagePressed(index);
+              },
+              currentPage: widget.currentPage,
+              index: widget.numberOfPages,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SinglePage extends StatelessWidget {
+  const _SinglePage({
+    Key? key,
+    required this.onPagePressed,
+    required this.currentPage,
+    required this.index,
+  }) : super(key: key);
+
+  final Function(int index) onPagePressed;
+  final int currentPage;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: InkWell(
+        onTap: () => onPagePressed(index),
+        child: Container(
+          height: 30,
+          width: 30,
+          color: Theme.of(context).cardColor,
+          child: Center(
+            child: FittedBox(
+              child: Text(
+                '$index',
+                style: TextStyle(
+                  color: index == currentPage ? Colors.white70 : Theme.of(context).disabledColor,
+                  fontWeight: index == currentPage ? FontWeight.bold : FontWeight.normal,
                 ),
               ),
-            );
-          },
+            ),
+          ),
         ),
       ),
     );
--- lib/ui/issues/issues_page.dart
+++ lib/ui/issues/issues_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tech_challenge_flutter/ui/common_widgets/common_pagination.dart';
 import 'package:tech_challenge_flutter/ui/issues/components/issues_list.dart';
 import 'package:tech_challenge_flutter/ui/issues/issue_bloc.dart';
+import 'package:tech_challenge_flutter/ui/utils/bloc_common.dart';
 
 class IssuesPage extends StatelessWidget {
   const IssuesPage({Key? key}) : super(key: key);
@@ -27,10 +28,17 @@ class IssuesPage extends StatelessWidget {
             const SizedBox(height: 12),
             BlocBuilder<IssueBloc, IssueState>(
               builder: (BuildContext context, IssueState state) {
-                return CommonPagination(
-                  numberOfPages: state.numberOfPages,
-                  onPagePressed: (index) => context.read<IssueBloc>().getIssuesForPage(pageNumber: index),
-                  currentPage: state.currentPage,
+                ScaffoldMessenger.of(context).clearSnackBars();
+
+                return Column(
+                  children: [
+                    CommonPagination(
+                      numberOfPages: state.numberOfPages,
+                      onPagePressed: (index) => context.read<IssueBloc>().getIssuesForPage(pageNumber: index),
+                      currentPage: state.currentPage,
+                    ),
+                    if (state.stateType == StateType.error) const SizedBox(height: 40),
+                  ],
                 );
               },
             ),
